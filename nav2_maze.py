cef5394b2fc0590056d691e8b71657dedab80dc8
Nick-1124 <2201124@sit.singaporetech.edu.sg>
2023-11-17 21:37:49 +0800

    Working Laser & rotation? still finding out how to use nav2 without using a map, i know use SLAM, i'm on it

--- nav2_maze.py
+++ nav2_maze.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 import rclpy
 import numpy as np
+import time
+import tf_transformations
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
+from nav2_simple_commander.robot_navigator import BasicNavigator
+from geometry_msgs.msg import PoseStamped
+from tf_transformations import quaternion_from_euler
+
 
 
 class MazeSolverNode(Node):
     def __init__(self):
         super().__init__('maze_solver_node')
         self.subscription = self.create_subscription(LaserScan, 'scan', self.laser_callback, 10)
-        self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.publisher_Twist = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.publisher_Pose = self.create_publisher(PoseStamped, 'move_base_simple/goal', 10)
+        self.nav = BasicNavigator()
+
 
     def laser_callback(self, msg):
         # Get the laser scan data
@@ -24,21 +33,41 @@ class MazeSolverNode(Node):
 
         # For example, print the average distance in each section
         for i in range(12):
-            print(f"Section {i + 1}: ", np.mean(sections[i]))
+            print(f"Section {12-i}: ", np.mean(sections[i]))
+        # twist_msg = self.calculate_movement(sections)
+        # self.publisher.publish(twist_msg)
+        self.calculate_movement(sections,self.nav)
 
-        twist_msg = self.calculate_movement(msg)
-        self.publisher.publish(twist_msg)
 
-    def calculate_movement(self, laser_data):
-        # Implement algorithm logic based on laser scan data
-        # Adjust robot's movements (e.g., turn left, turn right, move forward)
 
+    def calculate_movement(self, sections,navigator: BasicNavigator):
+        # 1. Find the index of the section with the maximum average distance
+        max_index = np.argmax([np.mean(section) for section in sections])
+        print(f"Max index: {max_index}")
+
+        # 2. Calculate the angle
+        # Assuming that the sections are evenly distributed over 360 degrees
+        angle = max_index * 2 * np.pi / 12
+        print(f"Angle: {angle}")
+
+        # 3. Create a nav2 pose stamp
+        pose = PoseStamped()
+        pose.header.stamp = navigator.get_clock().now().to_msg()
+        pose.header.frame_id = 'map'
+        q = quaternion_from_euler(0, 0, angle)
+        print(q)
+        pose.pose.orientation.x = q[0]
+        pose.pose.orientation.y = q[1]
+        pose.pose.orientation.z = q[2]
+        pose.pose.orientation.w = q[3]
+
+        # 4. Send the pose stamp
+        self.publisher_Pose.publish(pose)
+        navigator.goToPose(pose)                    
         
-        twist_msg = Twist()
-        # Example: Always move forward
-        twist_msg.linear.x = 0.5
-        return twist_msg
-    
+            
+
+            
 
 def main(args=None):
     rclpy.init(args=args)
@@ -46,6 +75,8 @@ def main(args=None):
     rclpy.spin(maze_solver_node)
     maze_solver_node.destroy_node()
     rclpy.shutdown()
+
+    
 
 if __name__ == '__main__':
     main()
